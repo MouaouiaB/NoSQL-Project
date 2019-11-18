a393e94237d5253a53f82a5c005b977dc3186f1d
Mouaouia Bouhtala <46076720+MouaouiaB@users.noreply.github.com>
2019-11-18 10:43:43 +0100

    Add routes

---
+++ src/routes/routes.js
@@ -0,0 +1,15 @@
+//import de controllers:
+const UserController = require('../controllers/users.controller');
+const CommentController = require('../controllers/comments.controller');
+const ThreadController = require('../controllers/threads.controller');
+
+//Hier komen te routes/ endpoints:
+//callback ()=>{...};
+module.exports = (app) => {
+//todo: User routes:
+
+//todo: Comment routes:
+
+//todo: Thread routes:
+
+};
