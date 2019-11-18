205d4931a75d60b5796f089a914461794903e51b
Mouaouia Bouhtala <46076720+MouaouiaB@users.noreply.github.com>
2019-11-18 10:37:59 +0100

    Add threadSchema to thread.js

---
+++ src/models/thread.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// import Schema's:
+const CommentSchema = require('./comment')
+
+const ThreadSchema = new Schema ({
+    userName: {type: String, required: true},
+    title: {type: String, required: true},
+    content: {type: String, required: true},
+    upVote: [{ type: String}],
+    downVote: [{ type: String}],
+    comments:[{CommentSchema}]
+});
+// connect/ koppel met mongoose, zodat mongo kan checken of thread al bestaat, als niet dan wordt er een collection gemaakt van threads door gebruik te maken van de schema:
+const Thread = mongoose.model('thread', ThreadSchema);
+// Export model/ schema:
+module.exports = Thread;
