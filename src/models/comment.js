0a54401d3e8c6fa39db9e6d24bee8b059a831474
Mouaouia Bouhtala <46076720+MouaouiaB@users.noreply.github.com>
2019-11-18 10:37:19 +0100

    add commentSchema

---
+++ src/models/comment.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema ({
+    userName: {type: String, required: true},
+    content: {type: String, required: true}
+});
+// connect/ koppel met mongoose, zodat mongo kan checken of comment al bestaat, als niet dan wordt er een collection gemaakt van comments door gebruik te maken van de schema:
+const Comment = mongoose.model('comment', CommentSchema);
+// Export model/ schema:
+module.exports = Comment;
