2425a231169bf542e322820d2462bcd3629629b0
Mouaouia Bouhtala <46076720+MouaouiaB@users.noreply.github.com>
2019-11-18 10:38:48 +0100

    Add a userSchema to user.js

---
+++ src/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema ({
+    userName: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    thread: [{ type: Schema.Types.ObjectId, ref: 'thread'}]
+});
+// connect/ koppel met mongoose, zodat mongo kan checken of user al bestaat, als niet dan wordt er een collection gemaakt van users door gebruik te maken van de schema:
+const User = mongoose.model('user', UserSchema);
+// Export model/ schema:
+module.exports = User;
