80952857346799736a7264169ec6f989a5e7902f
Mouaouia Bouhtala <46076720+MouaouiaB@users.noreply.github.com>
2019-11-19 12:18:00 +0100

    get get connection with MongoDB in app.js

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-module.exports = {
-    url: 'mongodb://localhost:27017/studdit'
-}
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+const routes = require('./src/routes/routes')
+
+mongoose.Promise = global.Promise;
+//connecten met de mongodb
+mongoose.connect('mongodb://localhost:27017/studdit', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => {
+        console.log('MongoDB connection established')
+    })
+    .catch(err => {
+        console.log('MongoDB connection failed')
+        console.log(err)
+    });
+
+
+app.use(bodyParser.json());
+routes(app);
+
+app.use((err, req, res, next) => {
+    res.status(422).send({error: err.message})
+})
+
+module.exports = app;
